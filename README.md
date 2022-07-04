875818dc042939e7d9377252d550a10a7fff022f
sixlighthouses <brendon.ward@sixlighthouses.com>
2022-07-04 12:30:40 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vue2-vuetify-vite-starter-mkurxw
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vue2-vuetify-vite-starter-mkurxw)
---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-app id="app">
+    <v-container>
+      <HelloWorld msg="Hello Vue 2 + Vuetify + Vite" />
+    </v-container>
+  </v-app>
+</template>
+
+<script>
+import { defineComponent } from '@vue/composition-api'
+import HelloWorld from './components/HelloWorld.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    HelloWorld,
+  },
+})
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
